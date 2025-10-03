39c932606fda373b3496a66e0e6492bd003bb33a
muhammadyaseen0877 <k240877@nu.edu.pk>
2025-10-03 23:53:01 +0500

    Question 3.cpp

--- Question 3.cpp
+++ Question 3.cpp
@@ -1,49 +1,79 @@
 #include <iostream>
 using namespace std;
 
-bool findpath(int i, int j, int n, int arr[][10]){
+bool findpath(int i, int j, int n,  int arr[][10], int result[][10]){
 	
-	bool reached=false;
+	if(i>=n || j>=n|| arr[i][j]==0|| i<0||j<0|| result[i][j]==1){
+		return false;
+	}
 	
-	if(i==j) return true;
+	result[i][j]=1;
 	
-	if(i<0 || j<0|| i>=n|| j>=n) return false;
-	
-	if(findpath(i, j+1, n, arr)==true){
-		arr[i][j]+=arr[i][j+1];
+	if((i==n-1 && j==n-1)){
+		cout<<"("<<i<<" "<<j<<")\n";
 		return true;
-		
+	}
+	
+	if(findpath(i+1,j, n,  arr, result)){
+		cout<<"("<<i<<" "<<j<<")\n";
+		return true;
 		
 	}
 	
-		if(findpath(i+1, j, n, arr)==true){
-		arr[i][j]+=arr[i+1][j];
+	if(findpath(i,j+1, n,  arr, result)){
+		cout<<"("<<i<<" "<<j<<")\n";
 		return true;
 		
+	}
+	
+	if(findpath(i-1,j, n,  arr, result)){
+		cout<<"("<<i<<" "<<j<<")\n";
+		return true;
 		
 	}
+	
+	if(findpath(i,j-1, n,  arr, result)){
+		cout<<"("<<i<<" "<<j<<")\n";
+		return true;
+		
+	}
+	
+	result[i][j]=0;
+	return false;
+	
 }
+
 int main(){
-	int n;
 	
-	int arr[10][10];
-	cout<<"Enter the dimmesnsions of the path: (square)";
-	cin>>n;
+	int n=7;
 	
-	cout<<"Enter the matrix: "<<endl;
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
-			cin>>arr[i][j];
+	int arr[10][10]={
+    {1,0,1,1,1,0,1},
+    {1,1,1,0,1,1,1},
+    {0,1,0,1,0,1,0},
+    {1,1,1,1,1,0,1},
+    {0,0,0,0,1,1,1},
+    {1,1,1,0,1,0,1},
+    {0,0,1,1,1,1,1}
+};
+	
+	int result[10][10]={0};
+	
+	cout<<"Backtracing path: "<<endl;
+	
+	if(findpath(0,0, n,  arr, result)){
+		cout<<"Path found for ice cream shop: "<<endl;
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				cout<<result[i][j]<<" ";
+			}
+			cout<<endl;
 		}
 	}
-	
-	int output[10][10];
-	if(findpath(0, 0, n, arr)==true){
-		
-		cout<<"lion reached sucessfully..."<<endl;
-		
+	else{
+		cout<<"No path found..."<<endl;
 	}
-	
 	
 	return 0;
 }
+
